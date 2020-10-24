b867acf8d026ba82bce46d1447dea7804813205c
contour66 <zamir.thind@gmail.com>
2020-10-23 21:15:54 -0700

    Updated Table View for track list on detail view

--- AlbumsList/AlbumsList/Album.swift
+++ AlbumsList/AlbumsList/Album.swift
@@ -16,10 +16,10 @@ class Album {
     var year: String
     var label: String
     var albums: [Album] = []
-//    var albums =  [Album]()
+    var songs: [String?] = []
     //MARK: Initialization
     
-    init?(photo: UIImage?,  album: String, artist: String,   year: String, label: String  ) {
+    init?(photo: UIImage?,  album: String, artist: String,   year: String, label: String, songs: [String]  ) {
         
         // Initialization should fail if there is no name or if the rating is negative.
         if album.isEmpty || artist.isEmpty || year.isEmpty || label.isEmpty  {
@@ -32,49 +32,45 @@ class Album {
         self.artist = artist
         self.year = year
         self.label = label
-     
+        self.songs = songs
+
+    }
+    
+    init?(photo: UIImage?,  album: String, artist: String,   year: String, label: String  ) {
         
+        // Initialization should fail if there is no name or if the rating is negative.
+        if album.isEmpty || artist.isEmpty || year.isEmpty || label.isEmpty  {
+            return nil
+        }
+        // Initialize stored properties.
+        self.photo = photo
+        self.album = album
+        self.artist = artist
+        self.year = year
+        self.label = label
+
     }
     
     
     
     
     class func createAlbumArray() -> [Album] {
-        let toolPhoto = UIImage(named: "tool")
-        let zepPhoto = UIImage(named: "zep")
-        let mjPhoto = UIImage(named: "mj")
-        
-        var albums: [Album] = []
-        
-        let album1 = Album(photo: toolPhoto, album: "Undertow", artist: "Tool", year: "1993", label: "zoo")
-        let album2 = Album.init(photo: zepPhoto, album: "Led Zepplin II", artist: "Led Zepplin", year: "1969", label: "Zoo" )
-        let album3 = Album.init(photo: mjPhoto, album: "Off The Wall", artist: "Michael Jackson", year: "1967", label: "atlantic" )
-        
-        
-        albums.append(album1!)
-        albums.append(album2!)
-        albums.append(album3!)
-        return albums
-    }
-    
-//    private func loadAlbums() {
 //        let toolPhoto = UIImage(named: "tool")
 //        let zepPhoto = UIImage(named: "zep")
 //        let mjPhoto = UIImage(named: "mj")
-//
-//
-//
-//        guard let album1 = Album.init(photo: toolPhoto, album: "Undertow", artist: "Tool", year: "1993", label: "zoo" )else {
-//            fatalError("unable to instantiate album")
-//        }
-//        guard let album2 = Album.init(photo: zepPhoto, album: "Led Zepplin II", artist: "Led Zepplin", year: "1969", label: "zoo" )else {
-//            fatalError("unable to instantiate album")
-//        }
-//        guard let album3 = Album.init(photo: mjPhoto, album: "Off The Wall", artist: "Michael Jackson", year: "1967", label: "atlantic" )else {
-//            fatalError("unable to instantiate album")
-//        }
-//
-//        albums += [album1, album2, album3]
-//
-//    }
+            let pinkPhoto = UIImage(named: "pinkfloyd")
+        var albums: [Album] = []
+        
+        //        let album1 = Album(photo: toolPhoto, album: "Undertow", artist: "Tool", year: "1993", label: "zoo")
+        //        let album2 = Album.init(photo: zepPhoto, album: "Led Zepplin II", artist: "Led Zepplin", year: "1969", label: "Zoo" )
+        //        let album3 = Album.init(photo: mjPhoto, album: "Off The Wall", artist: "Michael Jackson", year: "1967", label: "atlantic" )
+        let album4 = Album.init(photo: pinkPhoto, album: "Animals", artist: "Pink Floyd", year: "1977", label: "Harvest", songs: ["01:24 - Pigs on the Wing (Part 1)",    "17:04 - Dogs",     " 11:28 - Pigs (Three Different Ones)","10:20 - Sheep ", "01:24 - Pigs on the Wing (Part 2) "])
+        
+        //        albums.append(album1!)
+        //        albums.append(album2!)
+        //        albums.append(album3!)
+        albums.append(album4!)
+        return albums
+    }
+    
 }
--- AlbumsList/AlbumsList/DetailViewController.swift
+++ AlbumsList/AlbumsList/DetailViewController.swift
@@ -8,25 +8,28 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var albumLabel: UILabel!
     @IBOutlet weak var recordLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var table: UITableView!
     var album: Album?
+    var songs = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        songs = getSongs()
         setUI()
-       
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-      
     }
-
     
     func setUI(){
         albumLabel.text = album?.album
@@ -36,15 +39,53 @@ class DetailViewController: UIViewController {
         photoImageView.image = album?.photo
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func getSongs()->[String]{
+        let songs = album?.songs
+        for song in songs!{
+            print("Track: " + song!)
+        }
+        return songs! as! [String]
     }
-    */
+    
+    func printSongs(){
+        
+        for song in songs{
+            print("Track s " + song)
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "TrackCells"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? TrackCells else{
+            fatalError("The dequeued cell is not an instance of TrackListCell")
+        }
+        cell.trackLabel.text = songs[indexPath.row]
+        return cell
+    }
+    
 
 }
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
